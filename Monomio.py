4c45cfb1edbc73d1331587a61d0306bceb01d80d
Alejandro Ortega <aleortega849@gmail.com>
2021-12-05 16:14:30 -0500

    Clase monomio lista

--- Monomio.py
+++ Monomio.py
@@ -1,9 +1,11 @@
-# TODO: meth sumar y restar (check guia)
-
 class Monomio:
 
     # Constructor
     def __init__(self, coeficiente=0, exponente=0):
+        if coeficiente < 0:
+            raise Exception('El coeficiente no debe ser negativo')
+        elif exponente < 0:
+            raise Exception('El exponente no debe ser negativo')
         self.__coeficiente = coeficiente
         self.__exponente = exponente
 
@@ -17,14 +19,34 @@ class Monomio:
 
     @property
     def to_database(self):
-        dict = {}
-        dict['type'] = "Monomio"
-        dict['coeficiente'] = self.__coeficiente
-        dict['exponente'] = self.__exponente
-        return dict
+        obj = dict()
+        obj['type'] = "Monomio"
+        obj['coeficiente'] = self.__coeficiente
+        obj['exponente'] = self.__exponente
+        return obj
 
     def evaluar(self, valor):
-        return self.__coeficiente * pow(valor, self.__exponente)
+        return self.__coeficiente * (valor ** self.__exponent)
 
+    def sumar(self, monomio):
+        return self + monomio
+    
+    def restar(self, monomio):
+        return self - monomio
+
+    # Dunder methods
     def __repr__(self):
+        '''Representación'''
         return f"{self.__coeficiente} x^{self.__exponente}"
+
+    def __add__(self, value):
+        '''Habilitar y definir operador suma'''
+        if not isinstance(value, Monomio) or self.__exponente != value.__exponente:
+            raise Exception('Una instancia de Monomio solo puede operar con otro Monomio con el mismo exponente')
+        return Monomio(self.__coeficiente + value.__coeficiente, self.__exponente)
+
+    def __sub__(self, value):
+        '''Habilitar y definir operador substracción'''
+        if not isinstance(value, Monomio) or self.__exponente != value.__exponente:
+            raise Exception('Una instancia de Monomio solo puede operar con otro Monomio con el mismo exponente')
+        return Monomio(self.__coeficiente - value.__coeficiente, self.exponente)
