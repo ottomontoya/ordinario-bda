5e773e8c5ffe459018bd48978082da9fc31f8a8f
Alejandro Ortega <aleortega849@gmail.com>
2021-12-06 15:05:06 -0500

    scripts arreglados

--- Expresiones.py
+++ Expresiones.py
@@ -1,73 +1,22 @@
-from Polinomios import Polinomio
-from random import randint, uniform
+import random
+import pymongo
+from Monomio import Monomio
+from Polinomio import Polinomio
+from Expresion import Expresion
 
-# TODO: please check
-# operation methods needed?
-# evaluacion needed?
+URL = 'mongodb://ipy849:amerida@bdaordinario-shard-00-00.cn3bg.mongodb.net:27017,bdaordinario-shard-00-01.cn3bg.mongodb.net:27017,bdaordinario-shard-00-02.cn3bg.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-126nu3-shard-0&authSource=admin&retryWrites=true&w=majority'
+DB_NAME = 'BDA'
 
-class Expresiones:
+def Generate():
+    client = pymongo.MongoClient(URL)
+    db = client[DB_NAME]
+    cantidad_inserciones = random.randint(33000, 77000)
+    inserciones = list()
+    operaciones = ['+', '-', '*', '/']
+    for i in range(cantidad_inserciones):
+        operacion = operaciones[i % 4]
+        inserciones.append(Expresion.generate(operacion).to_database)
+    db.algebra.insert_many(inserciones)
 
-    def __init__(self, operando1 = None, operando2 = None):
-        self.__operando1 = operando1
-        self.__operando2 = operando2
-
-        if operando1 is None:
-            self.__operador1 = Polinomio().generatePoli()
-        if operando2 is None:
-            self.__operador2 = Polinomio().generatePoli()
-
-        self.__operacion = ""
-        self.__resultado = ""
-
-    @property
-    def operando1(self):
-        return self.__operando1
-
-    @property
-    def operando2(self):
-        return self.__operando2
-
-    @property
-    def to_database(self):
-        dict = {}
-        dict['type'] = "Expresion"
-        dict['operacion'] = self.__operacion
-        dict['operando1'] = self.__operando1.to_database
-        dict['operando2'] = self.__operando2.to_database
-        dict['resultado'] = self.__resultado
-        return dict
-
-    # Operation methods
-    def mult(self, p1, p2):
-        return p1 * p2
-
-    def suma(self, p1, p2):
-        return p1 + p2
-
-    def resta(self, p1, p2):
-        return p1 - p2
-
-    def div(self, p1, p2):
-        return p1 / p2
-
-    # Evaluate and set values for the polinoms
-    def evaluacion(self, valor):
-        Poly1 = self.__operando1.evaluar(valor)
-        Poly2 = self.__operando2.evaluar(valor)
-        op = randint(1, 4)
-        if(op == 1):
-            self.__operacion = "suma"
-            res = self.suma(Poly1, Poly2)
-            self.__resultado = res
-        if(op == 2):
-            self.__operacion = "resta"
-            res = self.resta(Poly1, Poly2)
-            self.__resultado = res
-        if(op == 3):
-            self.__operacion = "division"
-            res = self.div(Poly1, Poly2)
-            self.__resultado = res
-        if(op == 4):
-            self.__operacion = "multiplicacion"
-            res = self.mult(Poly1, Poly2)
-            self.__resultado = res
+if __name__ == '__main__':
+    Generate()
--- Polinomios.py
+++ Polinomios.py
@@ -1,50 +1,15 @@
-from pymongo import results
-from Monomio import Monomio
-from random import randint, uniform
+import random
+import pymongo
+from Polinomio import Polinomio
 
-# TODO:
-# prop grado
-# finish agregar
-# meth obtener (check guia)
+URL = 'mongodb://ipy849:amerida@bdaordinario-shard-00-00.cn3bg.mongodb.net:27017,bdaordinario-shard-00-01.cn3bg.mongodb.net:27017,bdaordinario-shard-00-02.cn3bg.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-126nu3-shard-0&authSource=admin&retryWrites=true&w=majority'
+DB_NAME = 'BDA'
 
-class Polinomio:
+def Generate():
+    client = pymongo.MongoClient(URL)
+    db = client[DB_NAME]
+    cantidad_inserciones = random.randint(50000, 75000)
+    db.algebra.insert_many([Polinomio.generate().to_database for _ in range(cantidad_inserciones)])
 
-    def __init__(self):
-        self.__monomios = []
-        self.__monomios.append(Monomio())
-
-    @property
-    def to_database(self):
-        dict = {}
-        dict['type'] = "Polinomio"
-        dict['monomios'] = [m.to_database for m in self.__monomios]
-        return dict
-
-    # FALTA GARANTIZAR LO DE QUE SOLO HAYA UN POLINOMIO CON CADA EXPONENTE
-    def agregar(self, monomio):
-        self.__monomios.append(monomio)
-
-    # Evaluate in regresion forms within the array list
-    def evaluar(self, valor):
-        resultado = 0
-        for monomio in self.__monomios:
-            resultado += monomio.evaluar(valor)
-        return resultado
-
-    # generate a unique polynomial
-    def generatePoli(self):
-        p = Polinomio()
-        NumPoli = randint(2, 10)
-        for i in range(NumPoli):
-            coef = uniform(-1000, 1000)
-            exp = randint(0, 500)
-            p.agregar(Monomio(coef, exp))
-        NumPoli = 0
-        return p
-
-    # Set the new representation form
-    def __repr__(self):
-        resultado = ""
-        for monomio in self.__monomios:
-            resultado += "+ " + str(monomio)
-        return resultado
+if __name__ == '__main__':
+    Generate()
